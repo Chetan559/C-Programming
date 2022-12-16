2d1ed0d966c4f68991103f2b6d1f89751b6a9f1e
Chetan Sharma <cschetan559@gmail.com>
2022-12-16 19:46:41 +0530

    practice

--- Learning C/primeno.c
+++ Learning C/primeno.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
 
+void some();
+
 int main(){
      
+
      int N,div;
      printf("Enter a no.\n");
      scanf("%d",&N);
      div=2;
      if(div<=N){
-         N%div=0 ? printf("NON PRIME NUMBER"): pr
-
-        }
+          some();    
+          }
      }
 
+void some(){
+     
 }
