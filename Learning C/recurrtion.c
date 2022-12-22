ad811e39dbf3525597db14305219d576cdffad1d
Chetan Sharma <cschetan559@gmail.com>
2022-12-22 22:10:44 +0530

    Create recurrtion.c

---
+++ Learning C/recurrtion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void add();
+
+int main(){
+      int a,b;
+    printf("Enter first number:-\n");
+    scanf("%d",&a);
+    printf("Enter second number:-\n");
+    scanf("%d",&b);
+    printf("The addition of these numbers is:%d\n",a+b);
+    return 0;
+}
