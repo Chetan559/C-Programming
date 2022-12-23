3c328cde9a2f4b101a7b911d7a036db458513e18
Chetan Sharma <cschetan559@gmail.com>
2022-12-23 19:10:16 +0530

    exam practice

--- Learning C/recurrtion.c
+++ Learning C/recurrtion.c
@@ -3,11 +3,16 @@
 void add();
 
 int main(){
-      int a,b;
+    add();
+    return 0;
+}
+
+void add(){
+    int a,b;
     printf("Enter first number:-\n");
     scanf("%d",&a);
     printf("Enter second number:-\n");
     scanf("%d",&b);
     printf("The addition of these numbers is:%d\n",a+b);
-    return 0;
+
 }
